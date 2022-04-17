a0b08cf4eb7f7c6396857c9d6296c9cb61cbdba6
Empress-of-Fire-and-Cats <tamara200148@gmail.com>
2022-04-17 21:13:40 +0700

    added zeros

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const btn__start = document.querySelector('.btn__start');
 const btn__stop = document.querySelector('.btn__stop');
 const btn__clear = document.querySelector('.btn__clear');
-const counter = document.querySelector('.counter');
 const img = document.querySelector('.img');
+const counter__hour = document.querySelector('.counter__hour');
+const counter__min = document.querySelector('.counter__min');
+const counter__sec = document.querySelector('.counter__sec');
 
 btn__start.addEventListener("click", startCounter);
 btn__stop.addEventListener("click", stopCounter);
@@ -14,18 +16,31 @@ let min = 0;
 let hour = 0;
 
 function startCounter() {
-    counter.textContent = `${hour}:${min}:${sec}`;
     intervalID = setInterval(() => {
         sec +=1;
-        if(sec == 5) {
+        if(sec == 15) {
             sec = 0;
             min++;
         }
-        if(min == 5) {
+        if(min == 15) {
             min = 0;
             hour++;
         }
-        counter.textContent = `${hour}:${min}:${sec}`;
+        if(hour >= 0 && hour < 10) {
+            counter__hour.textContent = '0' + hour;
+        } else {
+            counter__hour.textContent = hour;
+        }
+        if(min >= 0 && min < 10) {
+            counter__min.textContent = ':0' + min;
+        } else {
+            counter__min.textContent = ':' + min;
+        }
+        if(sec >= 0 && sec < 10) {
+            counter__sec.textContent = ':0' + sec;
+        } else {
+            counter__sec.textContent = ':' + sec;
+        }
     }, 1000);
     this.disabled = true;
     document.getElementById('img').style.display = 'block';
@@ -38,7 +53,9 @@ function stopCounter() {
 }
 
 function clearCounter() {
-    counter.textContent = '';
+    counter__hour.textContent = '';
+    counter__min.textContent = '';
+    counter__sec.textContent = '';
     sec = 0;
     min = 0;
     hour = 0;
